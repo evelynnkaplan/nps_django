836c0c5e5e2fdf45b1db212a89d61ae139ebcc1a
Evelynn Kaplan <evelynnkaplan@gmail.com>
2019-07-17 19:09:33 -0700

    setting os environ

--- nps_django/settings.py
+++ nps_django/settings.py
@@ -20,6 +20,9 @@ env = environ.Env(
 
 environ.Env.read_env()
 
+os.environ['DATABASE_USERNAME'] = env('DATABASE_USERNAME')
+os.environ['DATABASE_PASSWORD'] = env('DATABASE_PASSWORD')
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
