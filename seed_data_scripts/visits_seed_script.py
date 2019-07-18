e3a3d4a56eff1a11b35785a60839a317225184e7
Evelynn Kaplan <evelynnkaplan@gmail.com>
2019-07-18 13:11:18 -0700

    made seed script and seed data for visits model

---
+++ seed_data_scripts/visits_seed_script.py
@@ -0,0 +1,25 @@
+from faker import Faker
+import yaml 
+import random
+
+fake = Faker()
+
+visits = []
+
+for i in range(1, 61):
+  visit = {}
+  visit['model'] = 'nps_django.visit'
+  visit['pk'] = i
+  visit['fields'] = { 
+    'passholder': random.choice(range(1, 26)),
+    'date': fake.date_between(start_date="-2y", end_date="today"),
+    'park': random.choice(range(1, 62))
+    }
+  visits.append(visit)
+
+with open('visits.yaml', 'w') as outfile:
+    yaml.dump(visits, outfile, default_flow_style=False)
+
+
+
+
