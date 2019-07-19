883c2744ff642439067d39b1e9669e1d92b75830
Evelynn Kaplan <evelynnkaplan@gmail.com>
2019-07-19 10:24:41 -0700

    form validates that only senior lifetime passes can be submitted without exp date, all other pass types must have exp date to be submitted

--- nps_django/forms.py
+++ nps_django/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Pass
+from django.core.exceptions import ValidationError
 
 class PassForm(forms.ModelForm):
   class Meta:
@@ -8,3 +9,12 @@ class PassForm(forms.ModelForm):
     widgets = {
             'expiration_date': forms.SelectDateWidget()
         }
+  
+  def clean(self):
+    cleaned_data = super().clean()
+    expiration_date = cleaned_data.get("expiration_date")
+    type = cleaned_data.get("type")
+
+    if type != "Senior Lifetime" and not expiration_date:
+      error_message = "Please enter an expiration date. The only pass type without an expiration date are Senior Lifetime passes."
+      raise ValidationError(error_message, code='invalid')
